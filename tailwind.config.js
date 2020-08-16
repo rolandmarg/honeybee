1be108b3245803dfa0b3c1a2345fe7b36064aa1d
Rom Midnight <rmargve@gmail.com>
2020-08-16 16:19:41 +0400

    fix tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.jsx'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
